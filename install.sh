7dadb1e82d82c04a3f982307c66f8239493ea9ee
Adam Buxton <buxton.adam@gmail.com>
2016-07-06 09:33:54 +0100

    install script for ansible

---
+++ install.sh
@@ -0,0 +1,5 @@
+#! /usr/bin
+sudo easy_install pip
+sudo pip install ansible
+#If you are installing on OS X Mavericks, you may encounter some noise from your compiler. A workaround is to do the following:
+#sudo CFLAGS=-Qunused-arguments CPPFLAGS=-Qunused-arguments pip install ansible
